c019d0c770a90c2ef0c1d27c531edb47fae002fb
Kaivalen <97201905+Kaivalen@users.noreply.github.com>
2022-04-30 14:53:13 +0800

    Initial commit

---
+++ utils.py
@@ -0,0 +1,9 @@
+import settings # can't change settings/utils
+
+
+def height_prct(percentage):
+    return (settings.HEIGHT / 100) * percentage
+
+def width_prct(percentage):
+    return (settings.WIDTH / 100) * percentage
+
